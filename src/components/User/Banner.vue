175fd725f9323ae884b10cacd919a63099eed52c
Nighthree <jenny60538@gmail.com>
2020-03-09 22:40:15 +0800

    上傳圖片

--- src/components/User/Banner.vue
+++ src/components/User/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="carouselExampleCaptions" class="carousel slide choco" data-ride="carousel">
+  <!-- <div id="carouselExampleCaptions" class="carousel slide choco" data-ride="carousel">
     <ol class="carousel-indicators">
       <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
       <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
@@ -23,10 +23,10 @@
       <div class="carousel-item" style="z-index:0">
         <router-link to="/coupon">
           <img
-            src
+            src="../../assets/images/888.jpg"
             class="d-block w-100 bg-cover bannerHeight"
             alt
-            style="background-image:url(../../../../assets/images/青花瓷-大.jpg)"
+            style="background-image:url()"
           />
           <div class="carousel-caption bg-op6 p-2 px-md-4 py-md-4">
             <h5>開幕慶</h5>
@@ -40,7 +40,7 @@
             src
             class="d-block w-100 bg-cover bannerHeight"
             alt
-            style="background-image:url(../assets/images/生巧克力-大.jpg)"
+            style="background-image:url(../assets/images/\346\230\237\347\220\203-\345\244\247.jpg)"
           />
           <div class="carousel-caption bg-op6 p-2 px-md-4 py-md-4">
             <h5>全館 6 折</h5>
@@ -67,5 +67,37 @@
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
       <span class="sr-only">Next</span>
     </a>
+  </div>-->
+  <div>
+    <swiper :options="swiperOption" class="bannerHeight">
+      <swiper-slide><router-link to="/products" class="bannerLink bg-cover" title="立刻選購" style="background-image: url(https://raw.githubusercontent.com/Nighthree/ChocolateShop_vue_cli4/master/src/assets/images/888.jpg)"></router-link></swiper-slide>
+      <swiper-slide>Slide 2</swiper-slide>
+      <swiper-slide>Slide 3</swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+      <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>
+    </swiper>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      swiperOption: {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        }
+      }
+    };
+  }
+};
+</script>
