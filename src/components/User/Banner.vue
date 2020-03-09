692bb2a9689276fa88793050b14523eca2eeb014
Nighthree <jenny60538@gmail.com>
2020-03-09 22:24:13 +0800

    上傳 Banner 圖片

--- src/components/User/Banner.vue
+++ src/components/User/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <div id="carouselExampleCaptions" class="carousel slide choco" data-ride="carousel">
     <ol class="carousel-indicators">
       <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
       <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
@@ -12,7 +12,7 @@
             src
             class="d-block w-100 bg-cover bannerHeight"
             alt
-            style="background-image:url(https://vanillaknight.com/media/pics/20200102/1577952582.jpg)"
+            style="background-image:url(../../../../assets/images/星球-大.jpg)"
           />
           <div class="carousel-caption bg-op6 p-2 p-md-4">
             <h5>全館 8 折</h5>
@@ -21,16 +21,16 @@
         </router-link>
       </div>
       <div class="carousel-item" style="z-index:0">
-        <router-link to="/products">
+        <router-link to="/coupon">
           <img
             src
             class="d-block w-100 bg-cover bannerHeight"
             alt
-            style="background-image:url(https://vanillaknight.com/media/pics/20190814/1565775858.jpg)"
+            style="background-image:url(../../../../assets/images/青花瓷-大.jpg)"
           />
           <div class="carousel-caption bg-op6 p-2 px-md-4 py-md-4">
-            <h5>全館 6 折</h5>
-            <p class="carouselPSize mb-2">輸入折扣碼 666 即享購物車 6 折優惠</p>
+            <h5>開幕慶</h5>
+            <p class="carouselPSize mb-2">參加活動取得購物車 8 折優惠碼</p>
           </div>
         </router-link>
       </div>
@@ -40,7 +40,7 @@
             src
             class="d-block w-100 bg-cover bannerHeight"
             alt
-            style="background-image:url(https://vanillaknight.com/shop/pics/20180215/1518667878.jpg)"
+            style="background-image:url(../assets/images/生巧克力-大.jpg)"
           />
           <div class="carousel-caption bg-op6 p-2 px-md-4 py-md-4">
             <h5>全館 6 折</h5>
